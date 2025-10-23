e6168ca4e78cef7cb05ba9b1fe193fc246f5f51e
waliuddin1105 <waliahmed1105@gmail.com>
2025-10-23 23:09:12 +0500

    Added update profile route and updated overall user routes

--- backend/api/routes/users.py
+++ backend/api/routes/users.py
@@ -26,15 +26,11 @@ class LoginUser(Resource):
             return {
                 "Success" : "User login succesful!",
                 "access token" : access_token,
-                "user" : {
-                    "user_id" : attempted_user.user_id,
-                    "username" : attempted_user.username,
-                    "user role" : attempted_user.role.value
-                }
+                "user" :  attempted_user.to_dict()
             }, 200
          
         except Exception as e:
-            return {"Error": f"Unexpected error {str(e)}"}, 500
+            return {"Error": f"Unexpected Error {str(e)}"}, 500
 
 
 #/users/logout
@@ -52,34 +48,37 @@ class RegisterUser(Resource):
     @users_ns.doc('Register a user')
     @users_ns.expect(users_data)
     def post(self):
-        data = request.json
+        try:
+            data = request.json
 
-        if not data['username']:
-            return {"Error" : "Please enter a username"}, 400
-        if not data['email']:
-            return {"Error" : "Please enter a valid email address"}, 400
-        if not data['password']:
-            return {"Error" : "Please enter a valid password"}, 400
-        role = data.get('role')
-        if not role or role.lower() not in ['donor', 'creator', 'admin']:
-            return {"Error" : "Please select a valid user role"}, 400
-        
-        if Users.query.filter_by(username = data['username']).first():
-            return {"Error" : "Username already exists! Please choose a unique username"}, 400
-        if Users.query.filter_by(email = data ['email']).first():
-            return {"Error" : "An existing account is already associated with the provided email"}, 400
+            if not data['username']:
+                return {"Error" : "Please enter a username"}, 400
+            if not data['email']:
+                return {"Error" : "Please enter a valid email address"}, 400
+            if not data['password']:
+                return {"Error" : "Please enter a valid password"}, 400
+            role = data.get('role')
+            if not role or role.lower() not in ['donor', 'creator', 'admin']:
+                return {"Error" : "Please select a valid user role"}, 400
+            
+            if Users.query.filter_by(username = data['username']).first():
+                return {"Error" : "Username already exists! Please choose a unique username"}, 400
+            if Users.query.filter_by(email = data ['email']).first():
+                return {"Error" : "An existing account is already associated with the provided email"}, 400
 
-        new_user = Users(username = data['username'], email = data['email'], role = data['role'])
-        new_user.setPasswordHash(data['password'])
-        new_user.profile_image = data.get('profile_image', None)
+            new_user = Users(username = data['username'], email = data['email'], role = data['role'])
+            new_user.setPasswordHash(data['password'])
+            new_user.profile_image = data.get('profile_image', None)
 
-        db.session.add(new_user)
-        db.session.commit()
+            db.session.add(new_user)
+            db.session.commit()
 
-        return {
-            "Success" : "User registered succesfully!",
-            "user_id" : new_user.user_id
-        }, 200
+            return {
+                "Success" : "User registered succesfully!",
+                "user_id" : new_user.user_id
+            }, 200
+        except Exception as e:
+            return {"Error": f"Unexpected Error {str(e)}"}, 500
         
 #users/profile
 @users_ns.route('/profile/<int:user_id>')
@@ -87,19 +86,57 @@ class GetUserProfile(Resource):
     @users_ns.doc("Get user profile")
     @jwt_required
     def get(self, user_id):
-        attempted_user = Users.query.filter_by(user_id = user_id).first()
+        try:
+            attempted_user = Users.query.filter_by(user_id = user_id).first()
 
-        if not attempted_user:
-            return {"Error" : "User does not exist!"}, 404
+            if not attempted_user:
+                return {"Error" : "User does not exist!"}, 404
+            
+            return {
+                "Success" : True,
+                "user" :  attempted_user.to_dict()  
+            }, 200
+        except Exception as e:
+            return {"Error": f"Unexpected Error {str(e)}"}, 500
+
+#users/update-profile
+@users_ns.route('/update-profile/<int:id>')
+class UpdateUserProfile(Resource):
+    # @jwt_required
+    @users_ns.doc("Update user profile")
+    @users_ns.expect(users_data)
+    def put(self, id):
+        try:
+            user_to_update = Users.query.get(id)
+
+            if not user_to_update:
+                return {"Error" : "Such user does not exist"}, 404
         
-        return {
-            "success" : True,
-            "user" : {
-                "user_id" : attempted_user.user_id,
-                "username" : attempted_user.username,
-                "email" : attempted_user.email,
-                "role" : attempted_user.role.value,
-                "profile_image" : attempted_user.profile_image
-            }
-        }, 200
+            data = request.json
+            new_username = data.get(data['username'], user_to_update.username)
+            new_password = data.get(data['password'], user_to_update.password)
+            new_role = data.get(data['role'], user_to_update.role.value)
+            new_profile_image = data.get(data['profile_image'], user_to_update.profile_image)
 
+            if new_username == Users.query.filter_by(username = new_username).first():
+                return {"Error" : "Username already exists! Please choose a unique username"}, 400
+            
+            if new_role.lower() not in ['donor', 'creator', 'admin']:
+                return {"Error" : "Please select a valid user role"}, 400
+            
+            user_to_update.username = new_username
+            user_to_update.setPasswordHash(new_password)
+            user_to_update.role = new_role
+            user_to_update.profile_image = new_profile_image
+
+            db.session.commit()
+
+            return {
+                "Success" : f"User profile with id {user_to_update.user_id} updated succesfully",
+                "user" : user_to_update.to_dict()
+                
+            }, 200
+        
+        except Exception as e:
+            return {"Error": f"Unexpected Error {str(e)}"}, 500
+       
